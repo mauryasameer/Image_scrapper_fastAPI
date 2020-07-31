de0337c153f31c0a5d89f46962b1665c1ff7024e
username <sameer.apsdk@gmail.com>
2020-07-31 22:16:04 +0530

    added initial bindings for the templates and css files

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, requests
+from fastapi.templating import Jinja2Templates
+from starlette.requests import Request
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory='templates')
+app.mount('/static',StaticFiles(directory='static'),name = 'static' )
 @app.get('/')
-def root():
-    return '<h1>YO</h1>'
-
+def root(request: Request):
+    return templates.TemplateResponse('index.html',{'request':request})
 
 @app.get('/id/{id}')
 async def read_id(id:int,s:str=None):
